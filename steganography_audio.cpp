ac9fdd41b4931914b313e79a6cb9a9bdfc6e5c8f
kw1nt00 <matushin_savva@mail.ru>
2024-11-10 16:41:24 +0300

    change smthg bytes on HELLO

--- steganography_audio.cpp
+++ steganography_audio.cpp
@@ -7,19 +7,33 @@
 int main()
 {
     setlocale(LC_ALL, "ru");
-    std::ofstream file("Текст.txt", std::ios::in | std::ios::binary);
+    std::ofstream file("Departure_METRO_2033.mp3", std::ios::in | std::ios::binary);
 
     if (!file.is_open()) {
         std::cerr << "Ошибка открытия файла" << std::endl;
         return 1;
     }
 
-    char byte = 35;
-    file.seekp(2);
-    file.put(byte);
+    file.seekp(65536 + 5 * 4096);
+    file.put(72);
+    file.put(69);
+    file.put(76);
+    file.put(76);
+    file.put(79);
+    
     file.close();
 
     std::cout << "Ок" << std::endl;
+
+    std::ifstream file2("Departure_METRO_2033.mp3", std::ios::in | std::ios::binary);
+
+    file2.seekg(65536 + 5*4096);
+    std::cout << (char)file2.get();
+    std::cout << (char)file2.get();
+    std::cout << (char)file2.get();
+    std::cout << (char)file2.get();
+    std::cout << (char)file2.get();
+
 
     return 0;
 }
