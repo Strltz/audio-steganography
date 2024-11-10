eb84ab45cbd828889a2f099da2a77f138d127bd7
kw1nt00 <matushin_savva@mail.ru>
2024-11-10 14:03:35 +0300

    initial commit

---
+++ steganography_audio.cpp
@@ -0,0 +1,25 @@
+﻿// steganography_audio.cpp
+//
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    std::ofstream file("Текст.txt", std::ios::in | std::ios::binary);
+
+    if (!file.is_open()) {
+        std::cerr << "Ошибка открытия файла" << std::endl;
+        return 1;
+    }
+
+    char byte = 35;
+    file.seekp(2);
+    file.put(byte);
+    file.close();
+
+    std::cout << "Ок" << std::endl;
+
+    return 0;
+}
